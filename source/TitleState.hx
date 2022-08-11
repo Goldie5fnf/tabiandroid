8a566e999fa208de4e7aa11b4ca208036b867bb0
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-08-11 21:25:02 +0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -118,6 +118,7 @@ class TitleState extends MusicBeatState
 	var gfDance:FlxSprite;
 	var danceLeft:Bool = false;
 	var titleText:FlxSprite;
+        var heart:FlxSprite;
 
 	function startIntro()
 	{
@@ -166,12 +167,20 @@ class TitleState extends MusicBeatState
 		// logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
+		heart = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+		heart.frames = Paths.getSparrowAtlas('Menu_Heart');
+		heart.animation.addByPrefix('idle', "bopping_heart", 24);
+		heart.antialiasing = true;
+		heart.animation.play('idle');
+		heart.updateHitbox();
+		add(heart);
+
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
-		add(gfDance);
+		//add(gfDance);
 		add(logoBl);
 
 		titleText = new FlxSprite(100, FlxG.height * 0.8);
